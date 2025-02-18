1485440838a0564414d5aec7c393227994ec2308
edwardsemmanuel <edwardsemmanuel878@gmail.com>
2025-02-18 17:52:29 +0100

    counter fix

---
+++ index.js
@@ -0,0 +1,24 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick=
+function (){
+    if (count > 0){
+        count -= 1;
+        document.getElementById("countlabel").innerHTML = count;
+    }
+
+}
+
+
+document.getElementById("resetBtn").onclick = 
+function (){
+    count = 0;
+    document.getElementById("countlabel").innerHTML = count;
+}
+
+
+document.getElementById("increaseBtn").onclick = 
+function (){
+    count +=1;
+    document.getElementById("countlabel").innerHTML = count;
+}
